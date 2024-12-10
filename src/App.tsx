a15864af74508d72fd82f2d9ca91a58709672e74
Sasha Nekto <bidukoleksandr9@gmail.com>
2024-12-10 20:27:53 +0100

    return site

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+
 import { RouterProvider } from "react-router-dom";
 import { router } from './router/routes';
 
