0382cdc09c3ca890c21d6916c3e65b6ca4ad442d
Sasha Nekto <bidukoleksandr9@gmail.com>
2024-12-11 15:50:51 +0100

    return site

--- src/pages/menu/menu_page.tsx
+++ src/pages/menu/menu_page.tsx
@@ -7,7 +7,7 @@ const MenuContainer = styled.div`
 
 console.log('API_LOCAL', import.meta.env.VITE_API_LOCAL);
 console.log('API', import.meta.env.APP_API);
-console.log('API', import.meta.env.VITE_APP_TEST);
+console.log('visibleEnvTest', import.meta.env.VITE_APP_TEST);
 // Компонент меню
 const Menu = () => {
 
