180251211963891c13169961a33fbf1cd9109fad
Sasha Nekto <bidukoleksandr9@gmail.com>
2024-12-23 21:29:14 +0100

    check ADD DATA function

--- src/pages/login/login_page.tsx
+++ src/pages/login/login_page.tsx
@@ -1,22 +1,9 @@
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../../amplify_outputs.json';
-import type { Schema } from '../../../amplify/data/resource'
-import { generateClient } from 'aws-amplify/data'
 
 
 Amplify.configure(outputs);
-const client = generateClient<Schema>({
-  authMode: 'userPool',
-});
-
-
-  const createItem = async () => {
-    await client.models.ItemMenu.create({
-      title: window.prompt("Todo content?"),
-      isDone: false
-    })
-  }
 
 const Login = () => {
     return(
@@ -25,10 +12,9 @@ const Login = () => {
         <main>
           <h1>Hello {user?.username}</h1>
           <button onClick={signOut}>Sign out</button>
-          <button onClick={createItem}>Add new todo</button>
         </main>
       )}
-</Authenticator>
+    </Authenticator>
     )
 }
 
