08df80a072d59e3d019cd4069cec0d4a4ffd5a4f
Sasha Nekto <bidukoleksandr9@gmail.com>
2024-12-25 21:58:11 +0100

    test UploadFunction

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,8 +4,10 @@ export const storage = defineStorage({
   name: 'littlelemonStorage',
   access: (allow) => ({
     'public/*': [
-      allow.guest.to(['read', 'write', 'delete', ]),
-      allow.authenticated.to(['read', 'write', 'delete', ]) // additional actions such as "write" and "delete" can be specified depending on your use case
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read', 'write', 'delete', ]),
+      allow.groups(['admins']).to(['read', 'delete', 'write']),
+      allow.groups(['workers']).to(['read']),     
     ]
   })
 });
--- src/pages/about/about_page.tsx
+++ src/pages/about/about_page.tsx
@@ -1,15 +1,20 @@
 import { FileUploader } from '@aws-amplify/ui-react-storage';
 import '@aws-amplify/ui-react/styles.css';
+import { StorageImage } from '@aws-amplify/ui-react-storage';
 
 const About = () => {
   return (
-    <FileUploader
+    <div>
+      <FileUploader
       acceptedFileTypes={['image/*']}
       path="public/"
       autoUpload={false}
       maxFileCount={1}
       isResumable
     />
+    <StorageImage alt="image" path="public/Facebook_Logo_Primary.png"/>
+    </div>
+
   );
 };
 export default About;
