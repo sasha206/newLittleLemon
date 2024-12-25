9b688646acb37e814fb48eb188d8a4d0253dfd85
Sasha Nekto <bidukoleksandr9@gmail.com>
2024-12-25 17:51:00 +0100

    test UploadFunction

--- src/pages/about/about_page.tsx
+++ src/pages/about/about_page.tsx
@@ -1,42 +1,43 @@
-import { useState, useEffect } from "react";
-import type { Schema } from "../../../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
 import { Amplify } from 'aws-amplify';
 import outputs from '../../../amplify_outputs.json';
+import { useState } from 'react';
+import { uploadData } from 'aws-amplify/storage';
 
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
+const About = () => {
+  const [file, setFile] = useState<File | null>(null);
 
-
-
-export default function ItemList() {
-  const [items, setItems] = useState<Schema["ItemMenu"]["type"][]>([]);
-
-  const fetchItems = async () => {
-    const { data: items } = await client.models.ItemMenu.list();
-    setItems(items);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setFile(event.target.files[0]);
+    }
   };
 
-  useEffect(() => {
-    fetchItems();
-  }, []);
+  const handleUpload = async () => {
+    if (!file) {
+      alert('Please select a file before uploading.');
+      return;
+    }
 
-  const createItem = async () => {
-    await client.models.ItemMenu.create({
-      title: window.prompt("Todo content?"),
-    });
-    fetchItems();
-  }
+    try {
+      const result = await uploadData({
+        path: `picture-submissions/${file.name}`,
+        data: file,
+      });
+      console.log('Upload successful:', result);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  };
 
   return (
     <div>
-      <button onClick={createItem}>Add new item</button>
-      <ul>
-        {items.map(({ id, title}) => (
-          <li key={id}>{title}</li>
-        ))}
-      </ul>
+      <input type="file" onChange={handleChange} />
+      <button onClick={handleUpload}>Upload</button>
     </div>
   );
-}
+};
+
+export default About;
+
