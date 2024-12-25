08d9261d0d482e1251c0aec3668a4eb2adef505c
Sasha Nekto <bidukoleksandr9@gmail.com>
2024-12-25 20:12:46 +0100

    test UploadFunction

--- src/pages/about/about_page.tsx
+++ src/pages/about/about_page.tsx
@@ -1,43 +1,15 @@
-import { Amplify } from 'aws-amplify';
-import outputs from '../../../amplify_outputs.json';
-import { useState } from 'react';
-import { uploadData } from 'aws-amplify/storage';
-
-Amplify.configure(outputs);
+import { FileUploader } from '@aws-amplify/ui-react-storage';
+import '@aws-amplify/ui-react/styles.css';
 
 const About = () => {
-  const [file, setFile] = useState<File | null>(null);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files[0]) {
-      setFile(event.target.files[0]);
-    }
-  };
-
-  const handleUpload = async () => {
-    if (!file) {
-      alert('Please select a file before uploading.');
-      return;
-    }
-
-    try {
-      const result = await uploadData({
-        path: `picture-submissions/${file.name}`,
-        data: file,
-      });
-      console.log('Upload successful:', result);
-    } catch (error) {
-      console.error('Error uploading file:', error);
-    }
-  };
-
   return (
-    <div>
-      <input type="file" onChange={handleChange} />
-      <button onClick={handleUpload}>Upload</button>
-    </div>
+    <FileUploader
+      acceptedFileTypes={['image/*']}
+      path="public/"
+      autoUpload={false}
+      maxFileCount={1}
+      isResumable
+    />
   );
 };
-
 export default About;
-
