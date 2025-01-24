fea25b7117e9a088816a73a54a5e031bf89ffc3f
Sasha Nekto <bidukoleksandr9@gmail.com>
2025-01-24 21:16:54 +0100

    Try add group validation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -46,7 +46,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'userPool',
+    defaultAuthorizationMode: 'apiKey',
     apiKeyAuthorizationMode: { expiresInDays: 30 }
   }
 });
